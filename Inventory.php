0c8adbcb516aca6cbde94c0890d298293acc027d
lamde <fatandfeed@gmail.com>
2021-03-05 11:31:03 +0700

    Fix bug

--- Inventory.php
+++ Inventory.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php  include('template/header.php'); ?>
 <?php  include('database.php'); ?>
 <?php $inventorys = pg_query($conn, "SELECT * FROM inventory"); ?>
@@ -63,72 +62,4 @@
 
             </div>
             <!-- End of Main Content -->
-=======
-<?php  include('template/header.php'); ?>
-<?php  include('database.php'); ?>
-<?php $inventorys = pg_query($conn, "SELECT * FROM inventory"); ?>
-<?php
-    
-    if (isset($_GET['del'])) {
-        $id = $_GET['del'];
-        try {
-            pg_query($conn, "DELETE FROM inventory WHERE InventoryID = $id");
-            echo '<script>window.location.href = "inventory.php";</script>';
-        } catch (Exception $e) {
-            echo 'Message: ' .$e->getMessage();
-        }
-    }
-
-?>
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Inventory Table</h1>
-
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <a href="addInventory.php" class="btn btn-success active" role="button" aria-pressed="true">Add Inventory</a>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>ID</th>
-                                            <th>OfficeID</th>
-                                            <th>Quantity</th>
-                                            <th>ProductCode</th>
-                                            <th>More</th>
-                                            
-                                        </tr>
-                                    </thead>
-                                    
-                                    <tbody>
-                                    <?php while ($row = pg_fetch_row($inventorys)) { ?>
-                                        <tr>
-                                            <td><?php echo $row['InventoryID']; ?></td>
-                                            <td><?php echo $row['OfficeID']; ?></td>
-                                            <td><?php echo $row['Qty']; ?></td>
-                                            <td><?php echo $row['ProductCode']; ?></td>
-                                            <td>
-                                                <a href="addiventory.php?edit=<?php echo $row['InventoryID']; ?>" class="btn btn-primary active" >update</a>
-                                                <a href="inventory.php?del=<?php echo $row['InventoryID']; ?>" class="btn btn-danger active">Delete</a>
-                                            </td>
-                                        </tr>
-                                    <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
->>>>>>> 2cde603cbba9fe4417c5191f69648258d2a6b971
 <?php  include('template/footer.php'); ?> 
--- database.php
+++ database.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 # Heroku credential 
@@ -14,21 +13,4 @@ if (!$conn)
 {
   die('Error: Could not connect: ' . pg_last_error());
 }
-=======
-<?php
-
-# Heroku credential 
-$host_heroku = "ec2-54-146-73-98.compute-1.amazonaws.com";
-$db_heroku = "ddf8u650c3idu7";
-$user_heroku = "ncpbukpjfzqszt";
-$pw_heroku = "e7933fdf4496f06b8f50f0a79769a1cb33b16b40e0e087374a67b2b96f77836f";
-# Create connection to Heroku Postgres
-$conn_string = "postgres://ncpbukpjfzqszt:e7933fdf4496f06b8f50f0a79769a1cb33b16b40e0e087374a67b2b96f77836f@ec2-54-146-73-98.compute-1.amazonaws.com:5432/ddf8u650c3idu7";
-$conn = pg_connect($conn_string);
-
-if (!$conn)
-{
-  die('Error: Could not connect: ' . pg_last_error());
-}
->>>>>>> 2cde603cbba9fe4417c5191f69648258d2a6b971
 ?>
